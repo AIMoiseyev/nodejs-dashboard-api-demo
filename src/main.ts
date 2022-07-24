30eef19d2083ce1422a0a10379c8e506ebfd260d
Alexander Moiseyev <daklaktakpak@yandex.ru>
2022-07-24 07:07:55 +0300

    add base functionality

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import {App} from "./app";
+import {LoggerService} from "./logger/logger.service";
+import {UsersController} from "./users/users.controller";
+import {ExceptionFilter} from "./errors/exception.filter";
+import {Container, ContainerModule, interfaces} from "inversify";
+import {TYPES} from "./types";
+import {ILogger} from "./logger/logger.interface";
+import {IExceptionFilter} from "./errors/exception.filter.interface";
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+    bind<ILogger>(TYPES.ILogger).to(LoggerService)
+    bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter)
+    bind<UsersController>(TYPES.UsersController).to(UsersController)
+    bind<App>(TYPES.Application).to(App)
+})
+
+function bootstrap() {
+    const appContainer = new Container()
+    appContainer.load(appBindings)
+    const app = appContainer.get<App>(TYPES.Application)
+    app.init()
+    return {app, appContainer}
+
+
+}
+
+// const logger = new LoggerService()
+// const app = new App(logger, new UsersController(logger), new ExceptionFilter(logger))
+
+export const {app, appContainer} = bootstrap();
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+
+export const TYPES = {
+    Application: Symbol.for('Application'),
+    ILogger: Symbol.for('ILogger'),
+    UsersController: Symbol.for('UsersController'),
+    ExceptionFilter: Symbol.for('ExceptionFilter'),
+}
